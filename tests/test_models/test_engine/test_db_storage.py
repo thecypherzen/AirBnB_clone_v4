e55499f52fdce7a93e1c00d3079bef8c5a1c9106
Valentine Nyibiam <saanmoyolnyibiam@gmail.com>
2024-08-06 00:16:06 +0000

    style(tests:dbstorage): enforce pycodestyle

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -67,6 +67,7 @@ test_db_storage.py'])
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
 
+
 if models.storage_t == 'db':
     class TestFileStorage(unittest.TestCase):
         """Test the FileStorage class"""
