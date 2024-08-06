fc10d343f4eb759d4e6839f54c2731d7dbf76686
William Inyam <sw.inyam@outlook.com>
2024-08-06 00:35:49 +0000

    feat(models): add password security in basemodel and user

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -26,4 +27,9 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        ha = hashlib.new("md5")
+        passwd = kwargs.get("password")
+        if passwd is not None:
+            ha.update(passwd.encode())
+            kwargs["password"] = ha.hexdigest()
         super().__init__(*args, **kwargs)
