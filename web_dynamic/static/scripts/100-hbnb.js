bd219dfeac8c760ea855ecc58ae3790e65f8510a
William Inyam <sw.inyam@outlook.com>
2024-08-15 15:37:26 +0000

    feat(dynamic:scripts): filter places by location and amenities

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,33 +1,103 @@
 // global variables
-let checkedAmenities = {}
-let checkedLocations = {}
-let checkdStates = {}
+const checkedAmenities = {};
+const checkedCities = {};
+const checkedStates = {};
 
 // global functions
-// get a user from user_id
+/**
+ * updateLocations - updates h4 in locations filter
+ * based on selected cities and states
+ *
+ * Returns: null
+ */
+function updateLocations () {
+  // create stringed-list of selected states & cities
+  let list = '';
+  for (const value of Object.values(checkedStates)) {
+    if (list.length) {
+      list += ', ';
+    }
+    list += value;
+  }
+  for (const value of Object.values(checkedCities)) {
+    if (list.length) {
+      list += ', ';
+    }
+    list += value;
+  }
+  // append selected checkboxes to h4
+  $('div.locations h4:first').text(`${list}`);
+}
+
+/**
+ * updateAmenities - updates h4 in amenities filter
+ * based on selected amenities
+ *
+ * Returns: null
+ */
+function updateAmenities () {
+  // create stringed-list of selected states & cities
+  let list = '';
+  for (const value of Object.values(checkedAmenities)) {
+    if (list.length) {
+      list += ', ';
+    }
+    list += value;
+  }
+  // append selected checkboxes to h4
+  $('div.amenities h4').text(`${list}`);
+}
+
+/**
+ * getUser - fetches a user from api by id
+ * userId: the uuid of the user to fetch
+ *
+ * Returns: JSONified api response data
+ */
 async function getUser (userId) {
-    const url = `http://0.0.0.0:5001/api/v1/users/${userId}`;
-    const response = await fetch(url, {
-        headers: { 'Content-Type': 'application/json' }
-    });
-    const data = await response.json();
-    return (data);
+  const url = `http://0.0.0.0:5001/api/v1/users/${userId}`;
+  const response = await fetch(url, {
+    headers: { 'Content-Type': 'application/json' }
+  });
+  const data = await response.json();
+  return (data);
 }
 
-
-// fetch places from api (and filter)
-async function getPlaces(url, amenities=null) {
-    const res = await fetch(url, {
-        method: 'POST',
-        body: ! amenities ? JSON.stringify({}) :
-            JSON.stringify({"amenities": amenities}),
-        headers: { 'Content-Type': 'application/json' }
-    });
-    return await res.json();
+/**
+ * getPlaces - fetch places from api, based on filters
+ * (states): list of state_ids to filter results by
+ * (cities): list of city_ids to filter results by
+ * (amenities): list of amenities to filter results by
+ *
+ * Returns: Promise(resolves to JSONified api response data)
+ */
+async function getPlaces (url, states = null, cities = null, amenities = null) {
+  const data = {};
+  if (states) {
+    data.states = states;
+  }
+  if (cities) {
+    data.cities = cities;
+  }
+  if (amenities) {
+    data.amenities = amenities;
+  }
+  const res = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: { 'Content-Type': 'application/json' }
+  });
+  return await res.json();
 }
 
-
-// create an article from a place object
+/**
+ * createPlacesArticle - creates a single UI place <article>
+ *  +based on the place's object
+ *
+ * (placeObj): object of place to create
+ *
+ * Returns: the created html <article> tag
+ */
 async function createPlaceArticle (placeObj) {
   const article = $('<article></article>');
   // add headline and price_by_night to headline_wrapper
@@ -67,68 +137,92 @@ async function createPlaceArticle (placeObj) {
   return article;
 }
 
-// create places section
-async function createPlacesSection(amenities=null) {
-    const placesSection = $('<section></section>').addClass('places')
-          .html($('<h1>').text('Places'));
-    const allPlaces = await getPlaces('http://0.0.0.0:5001/api/v1/places_search/',
-                                      amenities);
-    const sortedPlaces = allPlaces.sort(sp => {
-        return sp.name;
-    });
-    for (const place of sortedPlaces) {
-        const placeArticle = await createPlaceArticle(place);
-        placesSection.append(placeArticle);
-    }
-    $('div.content').html(placesSection);
+/**
+ * createPlacesSection - creates UI places section based on elected filters
+ *  +and appends it to UI
+ *
+ * (states): list of state_ids to filter by
+ * (cities): list of city_ids to filter by
+ * (amenities): list of amenity_ids to filter by
+ *
+ * Returns: null
+ */
+async function createPlacesSection (states, cities, amenities) {
+  const placesSection = $('<section></section>').addClass('places')
+    .html($('<h1>').text('Places'));
+  const allPlaces = await getPlaces('http://0.0.0.0:5001/api/v1/places_search/',
+    states, cities, amenities);
+  const sortedPlaces = allPlaces.sort(sp => {
+    return sp.name;
+  });
+  for (const place of sortedPlaces) {
+    const placeArticle = await createPlaceArticle(place);
+    placesSection.append(placeArticle);
+  }
+  $('div.content').html(placesSection);
 }
 
-
-// Entry
+/**
+ * document.ready - Program entry point
+ *
+ * Returns: null
+ */
 $(document).ready(() => {
-  // FILTER SECTION HANDLER
-  // add listener to input of type checkbox
-  const selected = {};
-  $('input:checkbox').on('change', function () {
-      const liElem = $(this);
-      const liParent = liElem.parent().parent().parent();
-      if(liParent.is("div.amenities")){
-          console.log("Amenity item:", liElem);
-      } else {
-          console.log("Location: ", liElem);
+  /**
+   * Filter section handler
+   *
+   * Updates the <h4> tag of the Locations and Amenities filters
+   * +based on each check-box's checked or un-checked state
+   *
+   * Returns: null
+   */
+  $('input:checkbox').on('change', function (e) {
+    if ($(this).parent().parent().is('ul.popover')) {
+      const children = $(this).parent().children();
+      if (children.length === 1) {
+        // add or delete checked or unchecked `amenities`
+        if (!$(this).is(':checked')) {
+          delete checkedAmenities[$(this).attr('data-id')];
+        } else {
+          checkedAmenities[$(this).attr('data-id')] = $(this).attr('data-name');
+        }
+        updateAmenities();
+      } else if (children.length === 3) {
+        // add or delete checked  or unchecked `states`
+        if (!$(this).is(':checked')) {
+          delete checkedStates[$(this).attr('data-id')];
+        } else {
+          checkedStates[$(this).attr('data-id')] = $(this).attr('data-name');
+        }
+        updateLocations();
       }
- /*   const key = $(this).attr('data-id');
-    if ($(this).is(':checked')) {
-      selected[key] = $(this).attr('data-name');
     } else {
-      delete selected[key];
-    }
-
-    // create is to of selected checkboxes
-    let list = '';
-    for (const value of Object.values(selected)) {
-      if (list.length) {
-        list += ', ';
+      // add or delete checked or unchecked `cities`
+      if (!$(this).is(':checked')) {
+        delete checkedCities[$(this).attr('data-id')];
+      } else {
+        checkedCities[$(this).attr('data-id')] = $(this).attr('data-name');
       }
-      list += value;
+      updateLocations();
     }
-
-    // append selected checkboxes to h4
-    $('div.amenities h4').html(`${list}`); */
   });
 
-  // CHECK INPUT IF LI IS CLICKED
-    $(".popover li").on("click", function (e){
-        if(! $(e.target).is("input")){
-            if(! $(this).parent().is("ul.popover") ||
-               $(this).parent().parent().is("div.amenities")){
-                $($(this).children("input")[0]).click();
-            }
-        }
-    });
+  /**
+    * delegates filters checkbox click/unclick events to
+    * +their parent <li>
+    */
+  $('.popover li').on('click', function (e) {
+    if (!$(e.target).is('input')) {
+      if (!$(this).parent().is('ul.popover') ||
+               $(this).parent().parent().is('div.amenities')) {
+        $($(this).children('input')[0]).click();
+      }
+    }
+  });
 
-  // SERVER STATUS
-  // Update server status
+  /**
+   * Update API Status Badge in UI
+   */
   const apiBadge = $('#api_status');
   const url = 'http://0.0.0.0:5001/api/v1/status/';
   $.get(url, (res, xhrObj) => {
@@ -138,20 +232,18 @@ $(document).ready(() => {
     apiBadge.removeClass('available');
   });
 
-  // UPDATE PLACES FROM UI
+  /**
+   * create UI places section
+   */
   createPlacesSection();
 
-  // BUTTON CLICK EVENT HANDLER - FILTERS
-    $("button").on("click", (e) => {
-        const selectedAmenities = Array.from($(".filters").children(".amenities")
-                                         .children(".popover").children("li"))
-              .filter((listItem) => {
-                  return $(listItem).children("input").is(":checked");
-              }).map(activeItem => {
-                  return $(activeItem).children("input").attr("data-id");
-              });
-        createPlacesSection(selectedAmenities);
-        e.preventDefault();
-    });
-
+  /**
+   * Submit button click even handler
+   */
+  $('button').on('click', (e) => {
+    createPlacesSection(Object.keys(checkedStates),
+      Object.keys(checkedCities),
+      Object.keys(checkedAmenities));
+    e.preventDefault();
+  });
 });
